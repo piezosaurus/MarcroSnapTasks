b1498fa51185fce57029021b60ef52fa4924e845
ktam <kendra.tam8@gmail.com>
2023-03-16 15:52:39 -0400

    added open website task

--- app/src/main/java/com/example/marcrosnaptasks/MainActivity.kt
+++ app/src/main/java/com/example/marcrosnaptasks/MainActivity.kt
@@ -1,16 +1,23 @@
 package com.example.marcrosnaptasks
 
+import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
 import android.widget.Button
+import androidx.activity.result.contract.ActivityResultContracts
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val tasks = Tasks(applicationContext)
+        val graph: GraphView = findViewById(R.id.graph)
+        graph.setData(generateRandomDataPoints())
+
+        val tasks = Tasks(this@MainActivity, applicationContext)
 
         val msg1 = "1 sec timer"
         val secs = 1
@@ -20,22 +27,49 @@ class MainActivity : AppCompatActivity() {
         val min = 0
 
         val geo = Uri.parse("geo:47.6,-122.3")
-        val url = Uri.parse("https://piezosaurus.github.io/")
+        val url = "https://piezosaurus.github.io/"
+        val phoneNum = "6478237838"
 
         val task1: Button = findViewById(R.id.button1)
         task1.setOnClickListener {
-            tasks.timer(msg1, secs)
+//            tasks.timer(msg1, secs)
+            tasks.dialPhoneNumber(phoneNum)
         }
 
         val task2: Button = findViewById(R.id.button2)
         task2.setOnClickListener {
-            tasks.alarm(msg2, hour, min)
+//            tasks.alarm(msg2, hour, min)
+            tasks.website(url)
         }
 
         val task3: Button = findViewById(R.id.button3)
         task3.setOnClickListener {
-            tasks.spotify(url)
+            graph.setData(generateRandomDataPoints())
+//            tasks.spotify()
+//            val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+//            startActivityForResult(takePictureIntent, 1337)
         }
-
+    }
+    private fun generateRandomDataPoints(): List<DataPoint> {
+        val random = Random()
+        return (0..20).map {
+            DataPoint(it, random.nextInt(50) + 1)
+        }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- app/src/main/java/com/example/marcrosnaptasks/Tasks.kt
+++ app/src/main/java/com/example/marcrosnaptasks/Tasks.kt
@@ -1,23 +1,22 @@
 package com.example.marcrosnaptasks;
 
-import android.app.SearchManager
-import android.content.ComponentName
+import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.provider.AlarmClock
-import android.provider.MediaStore
 import android.util.Log
 import androidx.core.content.ContextCompat.startActivity
+import java.util.*
 
 
-public class Tasks(val context: Context) {
+public class Tasks(val activity: Activity, val context: Context) {
 
      public fun timer (msg: String, secs: Int) {
          val intent = Intent(AlarmClock.ACTION_SET_TIMER).apply {
              putExtra(AlarmClock.EXTRA_MESSAGE, msg)
              putExtra(AlarmClock.EXTRA_LENGTH, secs)
-             putExtra(AlarmClock.EXTRA_SKIP_UI, true)
+             putExtra(AlarmClock.EXTRA_SKIP_UI, false)
          }
          if (intent.resolveActivity(context.packageManager) != null) {
              context.startActivity(intent)
@@ -37,8 +36,28 @@ public class Tasks(val context: Context) {
         context.startActivity(intent)
         Log.i("PRINT", "alarm")
     }
+    public fun website (url: String) {
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+        if (intent.resolveActivity(context.packageManager) != null) {
+            context.startActivity(intent)
+        }
+        Log.i("PRINT", "website")
+    }
 
-    public fun spotify (url: Uri) {
+    fun dialPhoneNumber(phoneNumber: String) {
+        val intent = Intent(Intent.ACTION_CALL)
+        intent.data = Uri.parse("tel:$phoneNumber")
+//        val intent = Intent(Intent.ACTION_CALL).apply {
+//            data = Uri.parse("tel:$phoneNumber")
+//        }
+        if (intent.resolveActivity(context.packageManager) != null) {
+            context.startActivity(intent)
+        }
+    }
+
+    public fun spotify () {
+
+
 //        val intent = Intent(Intent.ACTION_VIEW).apply {
 //            data = geoLocation
 //        }
@@ -46,15 +65,15 @@ public class Tasks(val context: Context) {
 //            context.startActivity(intent)
 //        }
 
-        val intent = Intent(Intent.ACTION_VIEW, url)
-        if (intent.resolveActivity(context.packageManager) != null) {
-            context.startActivity(intent)
-        }
+//        val intent = Intent(Intent.ACTION_VIEW, url)
+//        if (intent.resolveActivity(context.packageManager) != null) {
+//            context.startActivity(intent)
+//        }
 
 //        val uri = "spotify:track:<spotify uri>"
 //        val launcher = Intent(Intent.ACTION_VIEW, Uri.parse(uri))
 //        context.startActivity(launcher)
-//
+
 //
 //        val intent = Intent(Intent.ACTION_MAIN)
 //        intent.action = MediaStore.INTENT_ACTION_MEDIA_PLAY_FROM_SEARCH
@@ -65,7 +84,31 @@ public class Tasks(val context: Context) {
 //        intent.putExtra(SearchManager.QUERY, artistName + " " + trackName)
 //        context.startActivity(intent)
 
+
+
         Log.i("PRINT", "fake spotify")
     }
 
+//    fun capturePhoto() {
+//        val storageDir = activity.getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+//        val image = File.createTempFile(
+//                "photo",  /* prefix */
+//                ".jpg",         /* suffix */
+//                storageDir      /* directory */
+//        )
+//        val photoURI = FileProvider.getUriForFile(
+//            Objects.requireNonNull(context),
+//            BuildConfig.APPLICATION_ID + ".provider", image);
+//
+////        val photoURI = FileProvider.getUriForFile(
+////            context,
+////            "com.example.android.fileprovider",
+////            image
+////        )
+//        val intent = Intent(MediaStore.ACTION)
+//        intent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+//
+//        startActivityForResult(activity, intent, 1, null)
+//        Log.i("PRINT", "photo")
+//    }
 }
